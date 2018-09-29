a045b02d50ec4e78dd8e87267cf12dbcb5eadda3
Janne Savolainen <janne.savolainen@houston-inc.com>
2018-09-29 10:47:37 +0300

    Make ideTools only export the public API

--- src/ideTools.js
+++ src/ideTools.js
@@ -1,46 +1,45 @@
 const vscode = require('./vscode');
 const replaceSelectionInEditorWith = require('./replaceSelectionInEditorWith');
 
-const ideTools = {
-  _getActiveTextEditor: function() {
-    return vscode.window.activeTextEditor || null;
-  },
+const currentDocumentLanguageIsSupported = () =>
+  getDocumentLanguage() === 'javascript';
 
-  _getDocumentLanguage: function() {
-    const activeTextEditor = this._getActiveTextEditor();
+const getDocumentLanguage = () => {
+  const activeTextEditor = getActiveTextEditor();
 
-    return activeTextEditor ? activeTextEditor.document.languageId : null;
-  },
-
-  currentDocumentLanguageIsSupported: function() {
-    return this._getDocumentLanguage() === 'javascript';
-  },
-
-  getSelectedText: function() {
-    const activeTextEditor = this._getActiveTextEditor();
-
-    if (!activeTextEditor) return null;
-
-    const {
-      start: selectionStart,
-      end: selectionEnd
-    } = activeTextEditor.selection;
-
-    return activeTextEditor.document.getText(
-      new vscode.Range(selectionStart, selectionEnd)
-    );
-  },
-
-  replaceSelection: function(value) {
-    const activeTextEditor = this._getActiveTextEditor();
-
-    const replaceSelectionInEditor = replaceSelectionInEditorWith({
-      selection: activeTextEditor.selection,
-      value
-    });
-
-    activeTextEditor.edit(replaceSelectionInEditor);
-  }
+  return activeTextEditor ? activeTextEditor.document.languageId : null;
 };
 
-module.exports = ideTools;
+const getSelectedText = () => {
+  const activeTextEditor = getActiveTextEditor();
+
+  if (!activeTextEditor) return null;
+
+  const {
+    start: selectionStart,
+    end: selectionEnd
+  } = activeTextEditor.selection;
+
+  return activeTextEditor.document.getText(
+    new vscode.Range(selectionStart, selectionEnd)
+  );
+};
+
+const getActiveTextEditor = () => vscode.window.activeTextEditor || null;
+
+const replaceSelection = value => {
+  const activeTextEditor = getActiveTextEditor();
+
+  const replaceSelectionInEditor = replaceSelectionInEditorWith({
+    selection: activeTextEditor.selection,
+    value
+  });
+
+  activeTextEditor.edit(replaceSelectionInEditor);
+};
+
+module.exports = {
+  currentDocumentLanguageIsSupported,
+  getSelectedText,
+  replaceSelection
+};
