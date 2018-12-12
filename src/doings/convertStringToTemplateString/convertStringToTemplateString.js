8e431caa73f606fef707f2ab1543db2c7121acb4
Janne Savolainen <janne.savolainen@houston-inc.com>
2018-12-12 13:56:06 +0200

    Rewrite the logic to convert string to template string

--- src/doings/convertStringToTemplateString/convertStringToTemplateString.js
+++ src/doings/convertStringToTemplateString/convertStringToTemplateString.js
@@ -1,7 +1,52 @@
-const templatizeString = require('templatize-string');
+const flow = require('lodash/fp/flow');
+const map = require('lodash/fp/map');
+const join = require('lodash/fp/join');
 
 module.exports = targetString => {
-  const templateString = templatizeString(targetString);
+  if (!targetString) return '';
 
-  return templateString.renderWrapped();
+  return convertToTemplateString(targetString);
 };
+
+const escapeVariablesWith = escapeVariablePlaceholder => targetString =>
+  targetString.replace(/(\s)*\+(\s)*/gim, escapeVariablePlaceholder);
+
+const unescapeVariablesWith = escapeVariablePlaceholder => targetString =>
+  targetString.replace(escapeVariablePlaceholder, '+');
+
+const removeLineBreaks = targetString => targetString.replace('\n', '');
+const removeTabs = targetString => targetString.replace('\t', '');
+const escapeVariablePlaceholder = '@@@';
+const escapeVariables = escapeVariablesWith(escapeVariablePlaceholder);
+const unescapeVariables = unescapeVariablesWith(escapeVariablePlaceholder);
+
+const splitToParts = targetString =>
+  targetString.split(escapeVariablePlaceholder);
+
+const parseString = targetString =>
+  targetString.substr(1, targetString.length - 2);
+
+const parseExpression = expression => '${' + expression + '}';
+
+const parsePart = part => {
+  const partIsExpression = !part.startsWith("'") && !part.startsWith('"');
+
+  if (partIsExpression) {
+    return parseExpression(part);
+  }
+
+  return parseString(part);
+};
+
+const renderWrapped = targetString => '`' + targetString + '`';
+
+const convertToTemplateString = flow(
+  removeLineBreaks,
+  removeTabs,
+  escapeVariables,
+  splitToParts,
+  map(parsePart),
+  join(''),
+  unescapeVariables,
+  renderWrapped
+);
--- src/doings/convertStringToTemplateString/convertStringToTemplateString.test.js
+++ src/doings/convertStringToTemplateString/convertStringToTemplateString.test.js
@@ -1,33 +1,43 @@
 describe('convertStringToTemplateString', () => {
-  let templatizeStringMock;
   let convertStringToTemplateString;
-  let renderWrappedStub;
 
   beforeEach(() => {
-    renderWrappedStub = jest.fn(() => '`some-template-string`');
-
-    templatizeStringMock = jest.fn(() => ({
-      renderWrapped: renderWrappedStub
-    }));
-
-    jest.resetModules().doMock('templatize-string', () => templatizeStringMock);
-
     convertStringToTemplateString = require('./convertStringToTemplateString');
   });
 
-  describe('when called', () => {
-    let actual;
+  [
+    {
+      input: "numberOfItems + ' items'",
+      expectedOutput: '`${numberOfItems} items`'
+    },
+    {
+      input: "'List contains ' + numberOfItems + ' items'",
+      expectedOutput: '`List contains ${numberOfItems} items`'
+    },
+    {
+      input:
+        "'Attempted to hit the endpoint: (' + contentPersonalizationResource + ') but encountered an error'",
+      expectedOutput:
+        '`Attempted to hit the endpoint: (${contentPersonalizationResource}) but encountered an error`'
+    },
+    {
+      input:
+        "performance.now() + ' session lock in ' + sessionKeepTime.toFixed(2) + ' m'",
+      expectedOutput:
+        '`${performance.now()} session lock in ${sessionKeepTime.toFixed(2)} m`'
+    },
+    {
+      input:
+        "'this ' + 'is ' + 'a ' + 'string. ' + testVariable + (testVariable ? 'foo' : 'bar')",
+      expectedOutput:
+        "`this is a string. ${testVariable}${(testVariable ? 'foo' : 'bar')}`"
+    },
+    { input: '', expectedOutput: '' }
+  ].forEach(({ input, expectedOutput }) => {
+    it(`when using "${input}" as input for convert, returns with "${expectedOutput}"`, () => {
+      const actual = convertStringToTemplateString(input);
 
-    beforeEach(() => {
-      actual = convertStringToTemplateString('some-string');
-    });
-
-    it('asks for template string', () => {
-      expect(templatizeStringMock).toHaveBeenCalledWith('some-string');
-    });
-
-    it('returns the string that has been turned to template string', () => {
-      expect(actual).toBe('`some-template-string`');
+      expect(actual).toBe(expectedOutput);
     });
   });
 });
